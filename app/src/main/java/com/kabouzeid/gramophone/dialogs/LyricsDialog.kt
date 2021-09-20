2612d31266a040d5c2885da953a8ab2e54044235
chr56 <_>
2021-09-20 15:50:55 +0800

    use RecyclerView in lyricsDialog

--- app/src/main/java/com/kabouzeid/gramophone/dialogs/LyricsDialog.kt
+++ app/src/main/java/com/kabouzeid/gramophone/dialogs/LyricsDialog.kt
@@ -4,9 +4,14 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
+import com.kabouzeid.gramophone.R
+import com.kabouzeid.gramophone.adapter.SimpleAdapter
 import com.kabouzeid.gramophone.model.lyrics.AbsLyrics
-import java.lang.StringBuilder
 import java.util.regex.Pattern
 
 /**
@@ -15,34 +20,31 @@ import java.util.regex.Pattern
 class LyricsDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        val rawText: Array<String> = requireArguments().getString("lyrics")!!.split(Pattern.compile("\\\\[nNrR]")).toTypedArray()
-        val builder = StringBuilder()
-        for (s in rawText) {
-            builder.append(s).append("\n")
-        }
-        val text = builder.toString()
+        val rawText: Array<String> = requireArguments().getString("lyrics text")!!.split(Pattern.compile("(\\\\[nNrR])|(\\r?\\n)")).toTypedArray()
+//        val builder = StringBuilder()
+//        for (s in rawText) {
+//            builder.append(s).append("\n")
+//        }
+//        val text = builder.toString()
 
-        return MaterialDialog(activity as Context)
-                .title(text = requireArguments().getString("title")!!)
-                .message(text = text)
+        val dialog = MaterialDialog(activity as Context)
+            .title(text = requireArguments().getString("title")!!)
+            .positiveButton { dismiss() }
+            .customView(R.layout.dialog_lyrics, horizontalPadding = true)
+        val recyclerView = dialog.getCustomView().findViewById<RecyclerView>(R.id.recycler_view_lyrics)
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = SimpleAdapter(rawText)
+
+        return dialog
     }
 
     companion object {
-//        @JvmStatic
-//        fun create(lyrics: Lyrics): LyricsDialog {
-//            val dialog = LyricsDialog()
-//            val args = Bundle()
-//            args.putString("title", lyrics.song.title)
-//            args.putString("lyrics", lyrics.text)
-//            dialog.arguments = args
-//            return dialog
-//        }
         @JvmStatic
         fun create(lyrics: AbsLyrics): LyricsDialog {
             val dialog = LyricsDialog()
             val args = Bundle()
             args.putString("title", lyrics.getTitle() as String)
-            args.putString("lyrics", lyrics.getText())
+            args.putString("lyrics text", lyrics.getText())
             dialog.arguments = args
             return dialog
         }
